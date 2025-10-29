395f195efe22b279aed3534a0d30170a28489539
James Zetlen <james.zetlen@meetdandy.com>
2025-10-29 06:21:15 -0500

    Initial Commit

---
+++ DandyNotifier/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  DandyNotifier
+//
+//  Created by James Zetlen on 10/29/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ DandyNotifier/DandyNotifierApp.swift
@@ -0,0 +1,17 @@
+//
+//  DandyNotifierApp.swift
+//  DandyNotifier
+//
+//  Created by James Zetlen on 10/29/25.
+//
+
+import SwiftUI
+
+@main
+struct DandyNotifierApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
